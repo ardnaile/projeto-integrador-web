26fa01b5e3b2bcfc17a3106b4db4772e63b50ac6
WallaceB2 <118089332+WallaceB2@users.noreply.github.com>
2024-03-24 20:00:53 -0300

    Iniciando tela de Quiz

--- Front-end/src/rotas/usuario/Quiz.jsx
+++ Front-end/src/rotas/usuario/Quiz.jsx
@@ -1,24 +1,19 @@
 import React from 'react'
+import BackgroundLayout from '../../background/VariosLayouts'
 
-
-function Quiz() {
-  return (
-    <div className = "bg-blue-900 text-white h-screen flex flex-col items-center justify-center shadow-lg"> 
-      <div className = "bg-black w-90 max-w-800 border border-white rounded-lg shadow-lg ">
-        <span className = "text-lg font-bold text-center items-center flex flex-col justify-center">Qual é o Pokemon de Ash ?</span>
-        <div className = "p-8 m-0 gap-4 box-sizing grid grid-cols-2 w-full">
-          <button className ="bg-white text-black px-20 py-4 rounded-lg mb-2 hover:bg-gray-200">Pikachu</button>
-          <button className ="bg-white text-black px-20 py-4 rounded-lg mb-2 hover:bg-gray-200">Grouthlite</button>
-          <button className ="bg-white text-black px-20 py-4 rounded-lg mb-2 hover:bg-gray-200">Chalizard</button>
-          <button className ="bg-white text-black px-20 py-4 rounded-lg mb-2 hover:bg-gray-200">Bulbassauro</button>
+function ConfirmarEstudante() {''
+    return (
+        <div className = "">
+        <BackgroundLayout backgroundImage={`url('./src/assets/img_fundo/fundo0.png')`}>
+            <div className = " grid h-36 flex-col items-end justify-center mx-auto text-black font-bold text-4xl py-2 px-4">LEGAL, VOCÊ É ESTUDANTE! É A SUA PRIMEIRA VEZ AQUI?</div>
+            <div className = 'grid grid-cols-2 h-screen flex-col items-center justify-center w-96 mx-auto gap-16'>
+                <button className = "bg-blue-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-full">SIM</button>
+                <button className = "bg-blue-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded-full">NÃO</button>
+            </div>
+        </BackgroundLayout>
         </div>
-      <div className ="flex justify-center mt-4 gap-2 p-2">
-        <button className ="bg-white text-black px-20 py-4 rounded-lg mb-2 hover:bg-gray-200">Começar Quiz!</button>
-        <button className ="bg-white text-black px-20 py-4 rounded-lg mb-2 hover:bg-gray-200">Próxima Pergunta</button>
-      </div>
-      </div>
-    </div>
-  )
+
+    )
 }
 
-export default Quiz
+export default ConfirmarEstudante
