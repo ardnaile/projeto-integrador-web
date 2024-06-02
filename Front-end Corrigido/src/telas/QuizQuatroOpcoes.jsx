7c94edc532741a164e705b73ad8cc517c571fd30
WallaceB2 <118089332+WallaceB2@users.noreply.github.com>
2024-06-02 19:10:46 -0300

    Refatorando tela de Quiz do código antigo

--- Front-end Corrigido/src/telas/QuizQuatroOpcoes.jsx
+++ Front-end Corrigido/src/telas/QuizQuatroOpcoes.jsx
@@ -1,33 +1,46 @@
-// nessa tela falta:
-//     - fazer um grid com as quatro QuizQuatroOpcoes
-//     - implementar uma funcao pra deixar só uma ser selecionada por vez
-//     - arrumar o pop-up
-
 import React, { useState } from "react";
-import background from "../fundos/fundo-principal.svg";
-import BotaoVoltar from "../components/BotaoVoltar";
-import { Link } from "react-router-dom";
-import BotaoDica from "../components/BotaoDica";
+import background from "../fundos/fundo-principal.svg"; // Importar o fundo
+import BotaoVoltar from "../components/BotaoVoltar"; // Importar o componente do botão de voltar
+import Timer from "../components/Timer"; // Importar o componente do timer
+import BotaoDica from "../components/BotaoDica"; // Importar o componente do botão de dica
 
 const QuizQuatroOpcoes = () => {
+  const [respostaSelecionada, setRespostaSelecionada] = useState(null);
+
+  const handleSelecionarResposta = (opcao) => {
+    setRespostaSelecionada(opcao);
+  };
 
   return (
-    <div className="relative w-full h-screen flex justify-center items-center">
-        <div className="absolute inset-0 overflow-hidden z-0">
-            <img src={background} alt="Background" className="object-cover w-full h-full" />
-        </div>
-        <div> <Link to='Categorias'> <BotaoVoltar/> </Link> </div>
-        <div> <BotaoDica/> </div>  
-        {/* <Timer/>   */}
+    <div
+      className="relative flex flex-col items-center justify-center h-screen"
+      style={{ backgroundImage: `url(${background})` }} // Adicionar o fundo
+    >
 
+      // Adicionar outros componentes
+      <BotaoVoltar /> 
+      <Timer /> 
+      <BotaoDica /> 
 
-        <h1 className="absolute items-center justify-center z-10 top-60">AQUI VAI O TEXTO DA QUESTÃO LAALALALA</h1>
+      <h1 className="text-center text-xl text-white font-bold mb-8">
+        AQUI VAI O TEXTO DA QUESTÃO LAALALALA
+      </h1>
 
-        
-        
+      <div className="grid grid-cols-2 gap-4">
+        {[1, 2, 3, 4].map((opcao) => (
+          <button
+            key={opcao}
+            className={`${
+              respostaSelecionada === opcao ? 'bg-gray-400' : 'bg-white'
+            } hover:bg-green-400 text-black font-bold px-4 py-4 border-b-2 hover:border-white-500 rounded-xl w-64`}
+            onClick={() => handleSelecionarResposta(opcao)}
+          >
+            Resposta {opcao}
+          </button>
+        ))}
+      </div>
     </div>
   );
 };
 
 export default QuizQuatroOpcoes;
-
