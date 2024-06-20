f2bea0bb94b74165803662eb762e8a63c6640fb5
ardnaile <elimottacardoso@gmail.com>
2024-06-20 10:50:05 -0300

    Rota /verAlunosDaTurma implementada

--- Back-end/src/main/java/projetoWebQuiz/Backend/Repositories/EstudanteRepository.java
+++ Back-end/src/main/java/projetoWebQuiz/Backend/Repositories/EstudanteRepository.java
@@ -1,7 +1,14 @@
 package projetoWebQuiz.Backend.Repositories;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import projetoWebQuiz.Backend.Models.Estudante;
 
+import java.util.List;
+
 public interface EstudanteRepository extends MongoRepository<Estudante, String> {
+
+    @Query("{ 'turma': ?0 }")
+    List<Estudante> findByNomeTurma(String turma);
+
 }
--- Back-end/src/main/java/projetoWebQuiz/Backend/Services/TurmaService.java
+++ Back-end/src/main/java/projetoWebQuiz/Backend/Services/TurmaService.java
@@ -1,9 +1,12 @@
 package projetoWebQuiz.Backend.Services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import projetoWebQuiz.Backend.Models.Estudante;
 import projetoWebQuiz.Backend.Models.Professor;
 import projetoWebQuiz.Backend.Models.Turma;
+import projetoWebQuiz.Backend.Repositories.EstudanteRepository;
 import projetoWebQuiz.Backend.Repositories.TurmaRepository;
 
 import java.util.List;
@@ -12,6 +15,8 @@ import java.util.List;
 public class TurmaService {
     @Autowired
     private TurmaRepository turmaRepository;
+    @Autowired
+    private EstudanteRepository estudanteRepository;
 
     // Create
     public Turma salvarTurma(Turma turma){ return turmaRepository.save(turma); }
@@ -19,6 +24,12 @@ public class TurmaService {
     // Read
     public List<Turma> verTodasTurmas() {
         return turmaRepository.findAll();
+    }
+
+    // Ver alunos da turma
+
+    public List<Estudante> verAlunosDaTurma(String nomeTurma){
+        return estudanteRepository.findByNomeTurma(nomeTurma);
     }
 
 }
