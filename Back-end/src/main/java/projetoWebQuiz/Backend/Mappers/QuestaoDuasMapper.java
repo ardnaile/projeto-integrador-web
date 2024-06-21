9733bcaf37906fa0b8a034ef52121e31d2a1241c
WallaceB2 <118089332+WallaceB2@users.noreply.github.com>
2024-06-20 22:09:56 -0300

    Colocando @Component em duas classes Mappers para a aplicação voltar a rodar.

--- Back-end/src/main/java/projetoWebQuiz/Backend/Mappers/QuestaoDuasMapper.java
+++ Back-end/src/main/java/projetoWebQuiz/Backend/Mappers/QuestaoDuasMapper.java
@@ -1,8 +1,10 @@
 package projetoWebQuiz.Backend.Mappers;
 
+import org.springframework.stereotype.Component;
 import projetoWebQuiz.Backend.Dtos.QuestaoDuasDto;
 import projetoWebQuiz.Backend.Models.QuestaoDuas;
 
+@Component
 public class QuestaoDuasMapper {
 
     public QuestaoDuasDto toDTO(QuestaoDuas questaoDuas){
--- Back-end/src/main/java/projetoWebQuiz/Backend/Mappers/QuestaoQuatroMapper.java
+++ Back-end/src/main/java/projetoWebQuiz/Backend/Mappers/QuestaoQuatroMapper.java
@@ -1,8 +1,10 @@
 package projetoWebQuiz.Backend.Mappers;
 
+import org.springframework.stereotype.Component;
 import projetoWebQuiz.Backend.Dtos.QuestaoQuatroDto;
 import projetoWebQuiz.Backend.Models.QuestaoQuatro;
 
+@Component
 public class QuestaoQuatroMapper {
 
     public QuestaoQuatroDto toDTO(QuestaoQuatro questaoQuatro){
